6ca048fda54ba64ed42fcf6cc5d0b53be6891274
thgoas <thgas95@gmail.com>
2022-05-14 21:37:47 -0300

    creater models products and page products_overview_page and add page in main.dart

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+          padding: const EdgeInsets.all(10),
+          itemCount: loadedProducts.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10),
+          itemBuilder: (ctx, i) => Text(loadedProducts[i].title)),
+    );
+  }
+}
